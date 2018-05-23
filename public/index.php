fdc0f97a4e435a3fd588f51c1a6548bf5a7400d3
Serov <serov-pa@yandex.ru>
2018-05-23 16:34:03 +0300

    add test action

--- public/index.php
+++ public/index.php
@@ -7,15 +7,19 @@ use Framework\Http\Router\Router;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
+
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 ### Initialization
+
 $routes = new RouteCollection();
+
 $routes->get('home', '/', new Action\HelloAction());
 $routes->get('about', '/about', new Action\AboutAction());
 $routes->get('blog', '/blog', new Action\Blog\IndexAction());
 $routes->get('blog_show', '/blog/{id}', new Action\Blog\ShowAction(), ['id' => '\d+']);
+
 $router = new Router($routes);
 
 ### Running
