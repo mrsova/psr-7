9cde4910b0b03a1c69b574f4e817a13ac189b153
Serov <serov-pa@yandex.ru>
2018-05-23 16:23:18 +0300

    add action

---
+++ src/App/Http/Action/AboutAction.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Action;
+use Zend\Diactoros\Response\HtmlResponse;
+
+class AboutAction
+{
+    public function __invoke()
+    {
+        return new HtmlResponse('I am a simple site');
+    }
+}
